1e5e4c66540650665dbbb43373c4334b3587c985
josempnena <jose.m.pena.diaz@gmail.com>
2017-11-14 18:23:17 +0100

    Fix phones in modal

--- app/assets/javascripts/international_phones.js
+++ app/assets/javascripts/international_phones.js
@@ -1,7 +1,11 @@
 //= require intlTelInput/intlTelInput
 //= require intlTelInput/utils
 
-$(function () {
+$(function () { international_phones_init() });
+
+$(document).on('shown.bs.modal', function () { international_phones_init() });
+
+function international_phones_init () {
     $('[data-intl-tel-input]').each(function () {
         var element = $(this);
         var hidden = element.parent().find('.international_phone_number');
@@ -12,4 +16,4 @@ $(function () {
             hidden.val(match[match.length - 1]);
         });
     });
-});
+}
