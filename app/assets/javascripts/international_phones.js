f9f1ce6fa1f2205042a93054599cf1116dfccbc7
jordi cucala <jordicucala@hotmail.com>
2018-03-07 17:57:12 +0100

    test country_code

--- app/assets/javascripts/international_phones.js
+++ app/assets/javascripts/international_phones.js
@@ -9,10 +9,12 @@ function international_phones_init () {
     $('[data-intl-tel-input]').each(function () {
         var element = $(this);
         var hidden = element.parent().find('.international_phone_number');
+	var hidden_phone_country_code =  element.parent().find('.phone_country_code');
         element.intlTelInput(element.data('intl-tel-input') || {});
 
         element.closest('form').submit(function () {
-            var match = element.intlTelInput('getNumber').match(/(\+\d+)(?=)/g);
+            var match = element.intlTelInput('getNumber').match(/(\+(xl)?\d+)(?=)/g);
+	    hidden_phone_country_code.val(element.intlTelInput('getSelectedCountryData').dialCode)
             hidden.val(match[match.length - 1]);
         });
     });
